758920c295add83f0976c657a2c8730a56ed4791
danieldinjian <danieldinjian9@gmail.com>
2026-05-18 14:01:45 -0500

    feat: add cleaning function

---
+++ cleartool.py
@@ -0,0 +1,14 @@
+import os
+
+# fileName = "tox.ini"
+
+def clearFunc(fileName):
+    finalList = []
+    with open(fileName, "r") as f:
+        splitedLists = f.read().splitlines()
+        finalList = [line.strip() for line in splitedLists if not line.strip().startswith("#")]
+
+    # print("\n".join(finalList))
+    new_data = "\n".join(finalList)
+    with open(fileName, "w") as file:
+        file.write(new_data)
