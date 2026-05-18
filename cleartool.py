fd488ae5f9eceaa97c6ff4b55b3049ce2c7d8e92
danieldinjian <danieldinjian9@gmail.com>
2026-05-18 14:06:48 -0500

    fix: remove strip

--- cleartool.py
+++ cleartool.py
@@ -6,9 +6,12 @@ def clearFunc(fileName):
     finalList = []
     with open(fileName, "r") as f:
         splitedLists = f.read().splitlines()
-        finalList = [line.strip() for line in splitedLists if not line.strip().startswith("#")]
+        finalList = [line for line in splitedLists if not line.strip().startswith("#")]
 
     # print("\n".join(finalList))
     new_data = "\n".join(finalList)
     with open(fileName, "w") as file:
         file.write(new_data)
+
+# fileName = "bottest.py"
+# clearFunc(fileName)
